488528b1dfc4737b59d16b51a1ee9165f844cc17
md-toybur-rahman <toyburrahman735@gmail.com>
2023-12-07 10:43:17 +0600

    complete accordion functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,24 @@ function mobileMenubarHandler (id) {
         menubar.classList.add('-left-[1024px]');
     }
 }
+
+function accordionHandler (accordion) {
+    const accordionSign = accordion.children[0].children[1];
+     console.log(accordionSign);
+     const accordionCard = accordion.children[1];
+     if(!accordionSign.classList.contains('rotate-45')){
+        if(accordionSign.classList.contains('accordion-collapse-sign')){
+            accordionSign.classList.add('rotate-45');
+        }
+        else{
+            accordionSign.classList.add('rotate-90');
+        }
+        accordionCard.classList.remove('hidden', 'h-0');
+        accordionCard.classList.add('mt-6')
+     }
+     else{
+        accordionSign.classList.remove('rotate-45');
+        accordionCard.classList.add('hidden', 'h-0');
+        accordionCard.classList.remove('mt-6')
+     }
+}
